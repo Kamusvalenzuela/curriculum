cd6583319c288b67494c98fbca0ce8a2044801a3
Kamus <gunz_franco@hotmail.com>
2023-04-01 18:45:11 -0300

    Fixed fetch API
    - Now the data on inmortaltower.html will load correctly
    - Dinámic check with configured interval (1 second)
    - No sé porqué hago las notas en inglés.

--- js/inmortalapi.js
+++ js/inmortalapi.js
@@ -1,39 +1,21 @@
-fetch('https://www.ascensiongamedev.com/resources/status.php?host=inmortaltower.servegame.com&port=5451')
-    .then(response => response.text())
-    .then(data => {
-        if (data === '0' || parseInt(data) > 0) {
-            // El servidor está en línea y devuelve un número mayor a cero
-            console.log('El servidor está en línea con ${data} jugadores');
-        } else if (data === '-1') {
-            // El servidor está inactivo y devuelve -1
-            console.log('El servidor está inactivo');
-        } else {
-            // El servidor devuelve un valor no esperado
-            console.log('Respuesta inesperada del servidor:', data);
-        }
-    })
-    .catch(error => {
-        // Se produjo un error al hacer la solicitud
-        console.error('Error al hacer la solicitud:', error);
-    });
+const estadoJuego = document.getElementById("estado-juego");
+const jugadoresEnLinea = document.getElementById("jugadores-en-linea");
 
+const url = "https://www.ascensiongamedev.com/resources/status.php?host=inmortaltower.servegame.com&port=5451";
 
-fetch('https://www.ascensiongamedev.com/resources/status.php?host=inmortaltower.servegame.com&port=5451')
-    .then(response => response.text())
-    .then(data => {
-        if (data === '0') {
-            // El servidor está inactivo
-            document.write('<span style="color: red;">El servidor está inactivo</span>');
-        } else if (parseInt(data) > 0) {
-            // El servidor está en línea y devuelve un número mayor a cero
-            document.write('<span style="color: green;">El servidor está en línea</span>');
-            document.write(` y hay ${data} jugadores conectados`);
-        } else {
-            // El servidor devuelve un valor no esperado
-            document.write('Respuesta inesperada del servidor:', data);
-        }
-    })
-    .catch(error => {
-        // Se produjo un error al hacer la solicitud
-        document.write('Error al hacer la solicitud:', error);
-    });
+setInterval(function () {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            if (data === "-1") {
+                estadoJuego.textContent = "⛔️ Apagado";
+                jugadoresEnLinea.textContent = `Ninguno`;
+            } else {
+                estadoJuego.textContent = "✅ Encendido";
+                jugadoresEnLinea.textContent = `${data}`;
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}, 1000);
